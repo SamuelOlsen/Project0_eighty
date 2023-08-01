1665f0d7a80500784c38ad7b61bc91e845b0a134
Samuel olsen <samuelolsen@live.dk>
2023-08-01 12:50:07 +0200

    update

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+public partial class Program
+{ 
+
+    public static void Main(string[] args)
+    {
+        try{
+            string file = args[0];
+            Console.WriteLine(MainChecker(file));
+        }
+        catch (IndexOutOfRangeException){
+            Console.WriteLine(MainChecker("outOfBound"));
+        }
+        catch(FileNotFoundException){
+            Console.WriteLine(MainChecker("FileNotFound"));
+        }
+    }
+
+        static int MainChecker(string file){
+            switch (file){
+                case "outOfBound":
+                    Console.WriteLine("Please provide a filename");
+                    return 1;
+                case "FileNotFound":
+                    Console.WriteLine("Could not open file");
+                    return 1;
+                default:
+                    CountLines(file);
+                    return 0;
+        }
+        }
+
+        static void CountLines(string file){
+            string[] readLines = File.ReadAllLines(file);
+            for(int i = 0; i < readLines.Length; i++){
+                string stringWithoutSpaces = readLines[i].Replace(" ", "");
+                if(stringWithoutSpaces.Length > 10){
+                    Console.WriteLine("Line: "+ i + " is too long! It has: " + stringWithoutSpaces.Length + " characters");
+                }
+            }
+        }
+}
